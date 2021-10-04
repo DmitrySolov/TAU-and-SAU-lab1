12c6657c2806c357a23cdafdf031cb13915593f0
DmitrySolov <dmitry_sm1th2@mail.ru>
2021-10-04 16:16:20 +0300

    add new file

--- lab1.py
+++ lab1.py
@@ -7,13 +7,18 @@ import colorama as color
 def choise ():
     inertialessUnitName = "Безынерционное звено"
     aperiodicUnitName = "Апериодическое звено"
-
+    integraUnitName = "Интегрирующее звено"
+    ideallyDFUnitName = "Идеальное дифференцирующее звено"
+    realDFUnitName = "Реальное дифференцирующее звено"
     neednewchoise = True
 
     while neednewchoise:
         userInput = input("Введите номер команды:\n"
                           "1 - " + inertialessUnitName + ';\n'
-                          "2 - " + aperiodicUnitName + '.\n')
+                          "2 - " + aperiodicUnitName + ';\n'
+                          "3 - " + integraUnitName + ';\n'
+                          "4 - " + ideallyDFUnitName + ';\n'
+                          "5 - " + realDFUnitName + '.\n')
         if userInput.isdigit():
             neednewchoise = False
             userInput = int(userInput)
@@ -21,6 +26,12 @@ def choise ():
                 name = 'Безынерционное звено'
             elif userInput == 2:
                 name = 'Апериодическое звено'
+            elif userInput == 3:
+                name = 'Интегрирующее звено'
+            elif userInput == 4:
+                name = 'Идиальное дифференцирующее звено'
+            elif userInput == 5:
+                name = 'Реальное дифференцирующее звено'
             else:
                 print('\nНедопустимое значение!')
                 neednewchoise = True
@@ -43,15 +54,21 @@ def getUnit(name):
                 unit = matlab.tf([k], [1])
             elif name == 'Апериодическое звено':
                 unit = matlab.tf([k], [t, 1])
+            elif name == 'Интегрирующее звено':
+                unit = matlab.tf([1], [t, 0])
+            elif name == 'Идиальное дифференцирующее звено':
+                unit = matlab.tf([t, 0], [1, 1])
+            elif name == 'Реальное дифференцирующее звено':
+                unit = matlab.tf([k, 0], [t, 1])
         else:
             print('\nПожалуйста,введите числовое значение!')
             neednewchoise = True
     return unit
 def graph(num, title, y, x):
-    pyplot.subplot(2,1, num)
+    pyplot.subplot(1,2, num)
     pyplot.grid(True)
     if title == 'Перехаодная характеристика':
-        pyplot.plot(x, y, 'purple')
+        pyplot.plot(x, y, 'red')
     elif title == 'Импульсная характеристика':
         pyplot.plot(x, y, 'green')
     pyplot.title(title)
@@ -61,10 +78,11 @@ unitName = choise()
 unit = getUnit(unitName)
 
 timeLine = []
-for i in range(0, 1000):
-    timeLine.append(i/1000)
+for i in range(0, 150):
+    timeLine.append(i)
 [y, x] = matlab.step(unit, timeLine)
 graph(1, 'Перехаодная характеристика', y, x)
+
 [y, x] = matlab.impulse(unit, timeLine)
 graph(2, 'Импульсная характеристика', y, x)
 pyplot.show()
