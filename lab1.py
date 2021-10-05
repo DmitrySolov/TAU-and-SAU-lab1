4942b90e14d68a3708bb8f6692e64fee77ce94cd
DmitrySolov <dmitry_sm1th2@mail.ru>
2021-10-05 15:48:06 +0300

    add new file

--- lab1.py
+++ lab1.py
@@ -1,27 +1,27 @@
-import matplotlib.pyplot as pyplot
-import control.matlab as matlab
-import numpy as numpy
-import math
-import colorama as color
+import matplotlib.pyplot as pyplot  # Обозначаю упрощенные модели команд
+import control.matlab as matlab  # Обозначаю упрощенные модели команд
+import numpy as numpy  # Импортирую библиотеку numpy
+import math  # Импортирую библиотеку math
+import colorama as color  # Импортирую библиотеку colorama
 
-def choise ():
-    inertialessUnitName = "Безынерционное звено"
-    aperiodicUnitName = "Апериодическое звено"
-    integraUnitName = "Интегрирующее звено"
-    ideallyDFUnitName = "Идеальное дифференцирующее звено"
-    realDFUnitName = "Реальное дифференцирующее звено"
+def choise (): # Создаю определенный синтаксис def для последующего создания функций
+    inertialessUnitName = "Безынерционное звено"  # Создаю оператора "Безынерционное звено"
+    aperiodicUnitName = "Апериодическое звено"  # Создаю оператора "Апериодическое звено"
+    integraUnitName = "Интегрирующее звено"  # Создаю оператора "Интегрирующее звено"
+    ideallyDFUnitName = "Идеальное дифференцирующее звено"  # Создаю оператора "Идеальное дифференцирующее звено"
+    realDFUnitName = "Реальное дифференцирующее звено"  # Создаю оператора "Реальное дифференцирующее звено"
     neednewchoise = True
 
     while neednewchoise:
-        userInput = input("Введите номер команды:\n"
-                          "1 - " + inertialessUnitName + ';\n'
+        userInput = input("Введите номер команды:\n"  # Команда служит для того чтобы пользователь самостоятельно выбирал интересующую его функцию 
+                          "1 - " + inertialessUnitName + ';\n'  # Нумерую команды 
                           "2 - " + aperiodicUnitName + ';\n'
                           "3 - " + integraUnitName + ';\n'
                           "4 - " + ideallyDFUnitName + ';\n'
                           "5 - " + realDFUnitName + '.\n')
-        if userInput.isdigit():
+        if userInput.isdigit():  # Проверяю содержит ли численное значение
             neednewchoise = False
-            userInput = int(userInput)
+            userInput = int(userInput)  # Превращаем значение к типу integer
             if userInput == 1:
                 name = 'Безынерционное звено'
             elif userInput == 2:
@@ -32,22 +32,22 @@ def choise ():
                 name = 'Идиальное дифференцирующее звено'
             elif userInput == 5:
                 name = 'Реальное дифференцирующее звено'
-            else:
+            else:  # Если пользователь набирает число с неприсвоенной оперцией, например - 6, тогда программа выводит строчку 'Недопустимое значение!'
                 print('\nНедопустимое значение!')
                 neednewchoise = True
-        else:
+        else:  # Если пользователь набирает другой символ не типа integer, например - r, тогда программа выводит строчку 'Пожалуйста,введите числовое значение!'
             print('\nПожалуйста,введите числовое значение!')
             neednewchoise = True
     return name
 
-def getUnit(name):
+def getUnit(name): # Создаю определенный синтаксис def для последующего создания функций
 
     neednewchoise = True
     while neednewchoise:
         neednewchoise = False
         k = input('Пожалуйста введите коэфициент "k": ')
         t = input('Пожалуйста введите коэфициент "t": ')
-        if k.isdigit() and t.isdigit():
+        if k.isdigit() and t.isdigit(): # Если пользователь вводит коэффициенты типа integer, тогда программа начинает рассчет по формулам ниже
             k = int(k)
             t = int(t)
             if name == 'Безынерционное звено':
@@ -64,13 +64,13 @@ def getUnit(name):
             print('\nПожалуйста,введите числовое значение!')
             neednewchoise = True
     return unit
-def graph(num, title, y, x):
+def graph(num, title, y, x): # Ввожу определённый синтаксис для постройки графиков
     pyplot.subplot(1,2, num)
     pyplot.grid(True)
     if title == 'Перехаодная характеристика':
         pyplot.plot(x, y, 'red')
     elif title == 'Импульсная характеристика':
-        pyplot.plot(x, y, 'green')
+        pyplot.plot(x, y, 'blue')
     pyplot.title(title)
     pyplot.ylabel('Амплитуда')
     pyplot.xlabel('Время (с)')
@@ -78,11 +78,29 @@ unitName = choise()
 unit = getUnit(unitName)
 
 timeLine = []
-for i in range(0, 150):
+for i in range(0, 10):
     timeLine.append(i)
 [y, x] = matlab.step(unit, timeLine)
-graph(1, 'Перехаодная характеристика', y, x)
+graph(1, 'Переходная характеристика', y, x)
 
 [y, x] = matlab.impulse(unit, timeLine)
 graph(2, 'Импульсная характеристика', y, x)
+pyplot.show()
+
+timeLine = []
+for i in range(0, 150):
+    timeLine.append(i)
+pyplot.subplot()
+pyplot.grid(True)
+mag, phase, omega = matlab.freqresp(unit, timeLine)
+pyplot.plot (mag)
+pyplot.title('АЧХ')
+pyplot.ylabel('Амплитуда')
+pyplot.xlabel('Угловая частота (рад/с)')
+pyplot.show()
+pyplot.grid(True)
+pyplot.plot (phase)
+pyplot.title('ФЧХ')
+pyplot.ylabel('Фаза')
+pyplot.xlabel('Угловая частота (рад/с)')
 pyplot.show()
